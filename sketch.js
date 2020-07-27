69ba078d3d535f8f6ef2e226374550ee9d4357d8
gargpriyansha <62380371+gargpriyansha@users.noreply.github.com>
2020-07-27 20:29:28 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+
+
+var ground,engine,box1,world;
 function setup() {
   createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+
+  engine = Engine.create ();
+  world = engine.world;
+
+
+  var ground_options={
+    isStatic :true
+  }
+  ground = Bodies.rectangle(200,290,400,10);
+
+World.add(world,ground);
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0);
+  
+  Engine.update(engine);
+
+  rectMode(CENTER);
+  RECT(ground.position.x,groun.position.y,400,10);
+
   drawSprites();
 }
